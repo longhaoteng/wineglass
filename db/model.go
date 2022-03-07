400ad11b9bd014db57bc86b878dcd5a775207486
longhaoteng <longhaoteng@kingsoft.com>
2022-03-07 17:52:28 +0800

    del misuse tag

--- db/model.go
+++ db/model.go
@@ -1,7 +1,7 @@
 package db
 
 type Model struct {
-	ID        int64 `json:"id" gorm:"primary_key" copier:"Id"`
+	ID        int64 `json:"id" gorm:"primary_key"`
 	CreatedAt int64 `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt int64 `json:"updated_at" gorm:"autoUpdateTime"`
 }
